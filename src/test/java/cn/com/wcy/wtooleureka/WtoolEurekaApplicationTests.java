43c3bb75cf7fa6ae1c250ff57a8820f395b130bf
吴春阳 <stvnsh@163.com>
2020-12-03 20:14:23 +0800

    构建项目

---
+++ src/test/java/cn/com/wcy/wtooleureka/WtoolEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.wcy.wtooleureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtoolEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
